1de8438f11011136338af08a797f2597e7c53816
dev1ous <60881111+dev1ous@users.noreply.github.com>
2021-09-28 03:10:38 +0200

    update

--- Runtime polymorphism and container view/Object.h
+++ Runtime polymorphism and container view/Object.h
@@ -12,20 +12,23 @@
 template<typename T, typename U>
 concept copy_constructor_for_ref = !std::same_as<std::remove_cvref_t<T>, U>;
 
-struct ibase {
+struct ibase 
+{
 	void(*draw)(std::any&);
 };
 
-template<typename ConcreteType>
-constexpr ibase vtable_for{
+template<std::semiregular ConcreteType>
+constexpr ibase vtable_for
+{
 	[](std::any& _storage) { std::any_cast<ConcreteType&>(_storage).draw(std::add_lvalue_reference<sf::RenderWindow>{}); },
 };
 
-struct Object
+class Object
 {
+public:
 	Object(copy_constructor_for_ref<Object> auto&& x) : m_storage(std::make_any(x)),
 		m_vtable(std::move<decltype(x)>(vtable_for), [](std::any& _storage)
-			{delete std::any_cast<std::remove_cvref_t<decltype(x)>>(&_storage); }) {}
+			{delete std::any_cast<std::remove_cvref_t<decltype(x)>>(&_storage); }) {} 
 
 	void draw(sf::RenderWindow&);
 
@@ -35,20 +38,35 @@ private:
 };
 
 template<std::semiregular T>
-struct composite : std::ranges::view_interface<composite<T>> {
-	composite() = default;
+class maybe_view : public std::ranges::view_interface<maybe_view<T>>
+{
+public:
+	maybe_view() = default;
 
-	composite(T x) : m_data(std::move(x)) {}
+	maybe_view(T x) : m_data(std::move(x)) {}
 
 	T const* begin() const noexcept {
 		return m_data ? &*m_data : nullptr;
 	}
+
 	T const* end() const noexcept {
 		return m_data ? &*m_data + 1 : nullptr;
 	}
 
+	void draw(sf::RenderWindow& w)
+	{
+		*this | std::views::transform(std::move(this->m_data.value().draw(w))) | std::views::join;
+	}
+
+	template<std::ranges::output_range TContainer>
+	constexpr auto to(TContainer const& c) 
+	{
+		auto x = *this | std::views::common;
+		return TContainer(x.begin(), x.end());
+	}
 private:
 	std::optional<T> m_data{};
 };
+
 #endif
 
