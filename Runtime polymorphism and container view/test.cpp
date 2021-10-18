b96a202ae05423be01b73481fe0ab7de679d0810
dev1ous <60881111+dev1ous@users.noreply.github.com>
2021-10-18 16:59:32 +0200

    update

--- Runtime polymorphism and container view/test.cpp
+++ Runtime polymorphism and container view/test.cpp
@@ -19,7 +19,10 @@ auto main() -> int
 	sf::RenderWindow w;
 	std::vector<Object> x{ my_class{} };
 	view_container view{ x };
-	auto i = view | std::views::transform([&w](auto obj)->decltype(obj) { co_yield obj; }) | std::views::join;
+	/*auto i = view | std::views::transform([&w](auto underlying_view) { return underlying_view 
+		| std::views::transform([&w](auto& obj) -> decltype(obj) { obj.draw(w); }); }) | std::views::join;*/
 
+	auto k = view | std::views::transform([](auto underlying_view) { return underlying_view; }) | std::views::join 
+		| std::views::transform([&w](auto& obj) -> decltype(obj) { obj.draw(w); });
 	return 0;
 }
